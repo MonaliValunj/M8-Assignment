8b37b336d55861203c4881fec0d0672f4873440a
Monali Valunj <monavalunj96@gmail.com>
2023-11-18 14:37:43 -0800

    initial commit

---
+++ js/script-array-practice.js
@@ -0,0 +1,51 @@
+//STEP 1
+// let FavMovies = ["A Beautiful Mind", "Interstellar","The Post","The Pianist",
+// "First to the Moon"];
+// console.log(FavMovies[1]);
+
+//STEP 2
+// let movies = Array(5)
+// movies[0] = "A Beautiful Mind"
+// movies[1] = "Interstellar"
+// movies[2] = "The Post"
+// movies[3] = "The Pianist"
+// movies[4] = "Celine"
+// console.log(movies[0])
+
+//STEP 3
+let movies = Array(5)
+movies[0] = "A Beautiful Mind"
+movies[1] = "Interstellar"
+movies[2] = "The Post"
+movies[3] = "The Pianist"
+movies[4] = "Celine"
+console.log(movies)
+
+
+//STEP 4
+
+//STEP 5
+
+//STEP 6
+
+//STEP 7
+
+//STEP 8
+
+//STEP 9
+
+//STEP 10
+
+//STEP 11
+
+//STEP 12
+
+//STEP 13
+
+//STEP 14
+
+//STEP 15
+
+//STEP 16
+
+//STEP 17
