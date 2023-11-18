d0ba88e1f0f56412888438e352ac4afdd36cb53d
Monali Valunj <monavalunj96@gmail.com>
2023-11-18 15:58:03 -0800

    array practive task 12 solved

--- js/script-array-practice.js
+++ js/script-array-practice.js
@@ -13,32 +13,151 @@
 // console.log(movies[0])
 
 //STEP 3
-let movies = Array(5)
-movies[0] = "A Beautiful Mind"
-movies[1] = "Interstellar"
-movies[2] = "The Post"
-movies[3] = "The Pianist"
-movies[4] = "Celine"
-console.log(movies)
+// let movies = Array(5)
+// movies[0] = "A Beautiful Mind"
+// movies[1] = "Interstellar"
+// movies[2] = "The Post"
+// movies[3] = "The Pianist"
+// movies[4] = "Celine"
+
+// movies.splice(2,0,"Lion")
+// console.log(movies.length)
 
 
 //STEP 4
+// let movies = []
+// movies[0] = "A Beautiful Mind"
+// movies[1] = "Interstellar"
+// movies[2] = "The Post"
+// movies[3] = "The Pianist"
+// movies[4] = "Celine"
+
+// movies.shift()
+// console.log(movies)
+
 
 //STEP 5
+// let movies = []
+// movies[0] = "A Beautiful Mind"
+// movies[1] = "Interstellar"
+// movies[2] = "The Post"
+// movies[3] = "The Pianist"
+// movies[4] = "Lion"
+// movies[5] = "Celine"
+// movies[6] = "Good Night Oppy"
+
+// for (let i in movies) {
+// 	console.log(movies[i]);
+// }
 
 //STEP 6
+// let movies = []
+// movies[0] = "A Beautiful Mind"
+// movies[1] = "Interstellar"
+// movies[2] = "The Post"
+// movies[3] = "The Pianist"
+// movies[4] = "Lion"
+// movies[5] = "Celine"
+// movies[6] = "Good Night Oppy"
+
+// for (let i of movies) {
+// 	console.log(i);
+// }
 
 //STEP 7
+// let movies = []
+// movies[0] = "A Beautiful Mind"
+// movies[1] = "Interstellar"
+// movies[2] = "The Post"
+// movies[3] = "The Pianist"
+// movies[4] = "Lion"
+// movies[5] = "Celine"
+// movies[6] = "Good Night Oppy"
+
+// movies.sort() 
+
+// for (let i of movies) {
+// 	console.log(i);
+// }
+
 
 //STEP 8
+// let movies = []
+// movies[0] = "A Beautiful Mind"
+// movies[1] = "Interstellar"
+// movies[2] = "The Post"
+// movies[3] = "The Pianist"
+// movies[4] = "Lion"
+// movies[5] = "Celine"
+// movies[6] = "Good Night Oppy"
+
+// let leastFavMovies = ["Drishaym2","Crazy Rich Asians","All Good Things"]
+
+// console.log("Movies I liked: \n\n")
+// for(var i = 0; i < movies.length; i++){
+//      console.log(movies[i]+" \n")
+// }
+
+// console.log("\n\nMovies I regret watching: \n\n")
+// for(var i = 0; i < leastFavMovies.length; i++){
+//      console.log(leastFavMovies[i]+" \n")
+// }
 
 //STEP 9
+// let movies = []
+// movies[0] = "A Beautiful Mind"
+// movies[1] = "Interstellar"
+// movies[2] = "The Post"
+// movies[3] = "The Pianist"
+// movies[4] = "Lion"
+// movies[5] = "Celine"
+// movies[6] = "Good Night Oppy"
+
+// let leastFavMovies = ["Drishaym2","Crazy Rich Asians","All Good Things"]
+// movies = movies.concat(leastFavMovies)
+// console.log(movies.sort().reverse())
+// for (let i of movies){
+//     console.log(i)
+// } 
+
 
 //STEP 10
 
+// let movies = []
+// movies[0] = "A Beautiful Mind"
+// movies[1] = "Interstellar"
+// movies[2] = "The Post"
+// movies[3] = "The Pianist"
+// movies[4] = "Lion"
+// movies[5] = "Celine"
+// movies[6] = "Good Night Oppy"
+
+// let leastFavMovies = ["Drishaym2","Crazy Rich Asians","All Good Things"]
+// movies = movies.concat(leastFavMovies)
+// movies.sort().reverse()
+// console.log("Last item is: "+movies.slice(-1))
+
+
 //STEP 11
 
+// let movies = []
+// movies[0] = "A Beautiful Mind"
+// movies[1] = "Interstellar"
+// movies[2] = "The Post"
+// movies[3] = "The Pianist"
+// movies[4] = "Lion"
+// movies[5] = "Celine"
+// movies[6] = "Good Night Oppy"
+
+// let leastFavMovies = ["Drishaym2","Crazy Rich Asians","All Good Things"]
+// movies = movies.concat(leastFavMovies)
+// movies.sort().reverse()
+// console.log("first item is: "+movies.shift())
+
+
 //STEP 12
+ let FavMovies = ["A Beautiful Mind", "Interstellar","The Post","The Pianist"]
+
 
 //STEP 13
 
